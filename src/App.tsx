3314e9db15b17ee7606c96902656c3d4cd39e79e
Webie <90978643+vimal-oneway@users.noreply.github.com>
2023-07-08 23:07:56 +0530

    1st page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app bg-[#242424] h-[100dvh] h-screen flex items-center justify-center flex-col text-white">
+      <h1 className="font-bold text-3xl md:text-5xl">VIMAL RAJ R</h1>
+      <p className="text-gray-200">A Full stack developer</p>
+
+      <p className="text-xs text-gray-300">This web is under development🚧</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
